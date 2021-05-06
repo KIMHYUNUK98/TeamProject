3f1db36dd49d1fece78e9dd06ded64d03790f57a
highlyko17@gmail.com <61534328+HyeliKo@users.noreply.github.com>
2021-05-06 19:18:32 +0900

    change to manager.h

--- manager.h
+++ manager.h
@@ -2,6 +2,7 @@
 
 void listStudent(Student *s, int index);
 int selectStudent(Student *s, int index);
+int selectStudentID(Student *s, int index);
 int selectMenu();
 int loadFile(Student *s); //파일 불러오기
 void saveData(Student *s, int index); //파일 저장하기
