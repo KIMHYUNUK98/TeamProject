d05380206e51f9f93c3d76f05fc37df4e741bc85
HYUNUK <21800201@handong.edu>
2021-05-06 16:35:01 +0900

    manager.c

---
+++ manager.c
@@ -0,0 +1,43 @@
+#include "attendance.h"
+
+void listStudent(Student *s, int index) {
+
+}
+
+int selectStudent(Student *s, int index) {
+
+}
+
+int selectMenu() {
+	int menu;
+	printf("\n******출석부 명단 관리******\n");
+	printf("1. 조회\n");
+	printf("2. 추가\n");
+	printf("3. 수정\n");
+	printf("4. 삭제\n");
+	printf("5. 저장\n");
+	printf("1. 조회\n");
+	printf("1. 조회\n");
+	printf("1. 조회\n");
+}
+
+int loadFile(Student *s) {
+
+}
+
+void saveData(Student *s, int index) {
+
+}
+
+void giveGrade(Student *s) {
+
+}
+
+void searchName(Student *s) {
+
+}
+
+void searchID(Student *s) {
+
+}
+
